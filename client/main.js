fc8b7dcd6eeb1906afa384ab82ad646d0baadb6f
Jason Gonzales <jason.gonzales23@gmail.com>
2016-04-04 07:50:40 -0700

    add entries collection and hook up all the things

--- client/main.js
+++ client/main.js
@@ -1,11 +1,11 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import { Budgets } from '../collections/budgets';
+import { Entries } from '../collections/entries';
 
 import './main.html';
 
-/*
-Template.home.events({
+Template.Home.events({
   "click #add-this": function(evt, templ) {
     var amount, amountInput, description, descriptionInput, timestamp;
     amountInput = templ.find("#expense-amount");
@@ -23,37 +23,46 @@ Template.home.events({
   }
 });
 
-Template.home.budget = function() {
-  var amounts, amountsTotal, currentMonthEntries, end, mostRecentBudget, now, nowDay, nowMonth, nowYear, start;
-  now = new Date();
-  nowYear = now.getFullYear();
-  nowMonth = now.getMonth();
-  nowDay = now.getDate();
-  start = new Date(nowYear, nowMonth, 1).getTime();
-  end = new Date(nowYear, nowMonth + 1, 1).getTime();
-  mostRecentBudget = Budgets.find({}, {
-    sort: {
-      createdAt: -1
-    },
-    limit: 1
-  }).fetch();
-  currentMonthEntries = Entries.find({
-    createdAt: {
-      $gte: start,
-      $lt: end
+Template.Home.helpers({
+  budget() {
+    var amounts, amountsTotal, currentMonthEntries, end, mostRecentBudget, now, nowDay, nowMonth, nowYear, start;
+    now = new Date();
+    nowYear = now.getFullYear();
+    nowMonth = now.getMonth();
+    nowDay = now.getDate();
+    start = new Date(nowYear, nowMonth, 1).getTime();
+    end = new Date(nowYear, nowMonth + 1, 1).getTime();
+
+    mostRecentBudget = Budgets.find({}, {
+      sort: {
+        createdAt: -1
+      },
+      limit: 1
+    }).fetch();
+
+    currentMonthEntries = Entries.find({
+      createdAt: {
+        $gte: start,
+        $lt: end
+      }
+    }).fetch();
+
+    if (currentMonthEntries.length > 0 ) {
+      amounts = _.pluck(currentMonthEntries, "amount");
+      amountsTotal = _.reduce(amounts, function(memo, num) {
+        return memo + num;
+      });
+      return mostRecentBudget[0].amount - amountsTotal;
+    } else {
+      return mostRecentBudget[0].amount;
     }
-  }).fetch();
-  if (currentMonthEntries.length > 0) {
-    amounts = _.pluck(currentMonthEntries, "amount");
-    amountsTotal = _.reduce(amounts, function(memo, num) {
-      return memo + num;
-    });
-    return mostRecentBudget[0].amount - amountsTotal;
-  } else {
-    return "not computed";
+
+    if (mostRecentBudget.length == 0) {
+      return 'not computed';
+    }
   }
-};
-*/
+});
+
 Template.Budget.events({
   "click #add-this": function(evt, templ) {
     var amount, amountInput, description, descriptionInput, timestamp;
